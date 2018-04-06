dca06af6ddc20b6e2445c7dfbb7738532b3f4f27
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-04-06 16:56:11 +0200

    Añadidos scripts mejorados para crontab
    Modificados los scripts enciende y apaga para ser utilizados por el script regarZona. Este ultimo sera el utilizado para seleccionar la zona de riego así como la duración de este.

---
+++ scripts/enciende.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var io = require('../node_modules/socket.io-client');
+var socket = io.connect('http://localhost:8080', {reconnect: true});
+
+// Add a connect listener
+socket.on('connect', function (socket) {
+    console.log('Connected!');
+});
+socket.on('disconnect', function (socket) {
+    console.log('Desconectando!');
+});
+socket.emit(process.argv[2], 1);
+
+
+
+
+
+
